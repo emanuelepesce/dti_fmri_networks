3c1e0341ef6ca3d40f528f49d71c2b7d04a1169b
emanuelepesce.ep@gmail.com <-->
2015-11-08 11:29:58 +0100

    experiments propagation dti

--- R/propagation/driver_propdata.R
+++ R/propagation/driver_propdata.R
@@ -3,11 +3,11 @@ library(igraph)
 source("./analysis_prop_func.R", chdir = T)
 
 
-# ================================ initialization ==============================
+# ================================ Initialization ==============================
 path.base <- "./../../data/results/propagation/rand_walk_complete/borda_sw_004/server_fmri_complete_r1/"
-pathIn.ctrl <- "./random_walk/prop_ctrl_dti_r1.RData"
-pathIn.sla2 <- "./random_walk/prop_sla2_dti_r1.RData"
-pathIn.sla3 <- "./random_walk/prop_sla3_dti_r1.RData"
+pathIn.ctrl <- "./random_walk/borda/fmri/prop_ctrl_fmri_r67.RData"
+pathIn.sla2 <- "./random_walk/borda/fmri/prop_sla2_fmri_r67.RData"
+pathIn.sla3 <- "./random_walk/borda/fmri/prop_sla3_fmri_r67.RData"
 
 pathIn.graph_ex <- ("./../../data/results/borda_example_graph.graphml")
 g <- read.graph(pathIn.graph_ex, format = "graphml")
@@ -24,4 +24,5 @@ load(pathIn.sla3)
 res.prop[[3]] <- data.prop
 
 prop_analysys(res.prop, freq.k = 10, g, pathOut.obj)
-  
+load(pathOut.obj)
+analysis.prop
